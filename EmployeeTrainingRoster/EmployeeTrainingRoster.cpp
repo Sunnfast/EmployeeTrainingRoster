1fa3d5ac9acb48f417a244d1c6cefcd1899b04af
Simone <simone.k.christen@gmail.com>
2022-07-10 17:11:13 -0700

    updated from replit

--- EmployeeTrainingRoster/EmployeeTrainingRoster.cpp
+++ EmployeeTrainingRoster/EmployeeTrainingRoster.cpp
@@ -3,25 +3,21 @@
 
 #include <iostream>
 #include <string>
-#include <algorithm>;
+#include <algorithm>
 using namespace std;
 
-int employeeArray [6] = {1};
-int emp_index;
-int id;
-int rosterSize;
-int searchkey;
+int employeeArray[6] = { 420, 69 };
+int emp_index = 1;
+
+
+int rosterSize = sizeof(employeeArray) / 4; // get number of bytes and divide by 4 to get size of int array
 
 int add(int array[], int emp_index, int employeeid)
 {
-    cout << "Enter the employee ID that you wish to add to the roster" << "\n" << "and then add the index on the roster where you want to place them.";
-    cin >> id;
-    cin >> emp_index;
-
-    cout << "the id is " << id <<" and the emp_index is " << emp_index;
-
+    employeeArray[emp_index] = employeeid;
+    cout << "Employee ID # " << employeeid << " is set to index " << emp_index << "\n";
     return 0;
-}
+};
 
 int binarySearch(int array[], int key, int l, int r)
 {
@@ -49,7 +45,6 @@ int binarySearch(int array[], int key, int l, int r)
         //We found the key 
         else
         {
-
             return count;
         }
     }
@@ -57,56 +52,108 @@ int binarySearch(int array[], int key, int l, int r)
     //Key was not found  
 
     return -1;
-}
+};
 
-int search(int array[], int size, int searchkey)
+bool search(int array[], int size, int searchkey)
 {
-    sort(array, array + size);
+    int len = sizeof(employeeArray) / sizeof(employeeArray[0]);
 
-    //receives input array along with left and right indexes
-    //Returns the index of the search key if it is found 
-    //otherwise it returns -1 
+    // cout<<"Before sorting array : ";
+    /* for(int i=0; i<len; i++)
+    {
+      cout << " " << employeeArray[i];
+    } */
 
-    //check to see if sorts in place or produces new array sorted
-   
-    binarySearch(employeeArray, searchkey, /*left, right */);
+    std::sort(employeeArray, employeeArray + len);
+    int sortSize = len - 1;
+    int sortedArray[sortSize];
+
+    // cout  << "\n\nAfter sorting array : ";
+    for (int i = 0; i < len; i++)
+    {
+        //  cout << " " << employeeArray[i];
+        sortedArray[i] = employeeArray[i];
+        // cout << " " << sortedArray[i];
+    }
+
+    int left = 0;
+    int right = rosterSize - 1;
+
+
+    int outcome = binarySearch(employeeArray, searchkey, 0, rosterSize - 1);
+
+    if (outcome == -1)
+    {
+        // cout << "searchkey not found.";
+        return false;
+    }
+    else {
+        // cout << "searchkey found.";
+        return true;
+    }
+
 
 }
 
 int main()
 {
-    add(employeeArray, emp_index, id);
-    search(employeeArray, rosterSize, searchkey);
+    int id;
+    int searchkey;
+    int menuChoice;
+
+    cout << "MAIN MENU" << "\n" << "Select:" << "\n" "1 to add an employee to the roster, or" << "\n" << "2 to search the roster." << "\n";
+
+    cin >> menuChoice;
+
+    if (menuChoice == 1)
+    {
+        cout << "Adding employees, type -1 to quit." << "\n" << "Type in an employee ID # to add to the roster." << "\n";
+
+        while (emp_index < rosterSize)
+        {
+            cin >> id;
+
+            if (id != -1)
+            {
+                add(employeeArray, emp_index, id);
+                emp_index++;
+                cout << "Enter another employee ID." << "\n";
+                cout << "Checking employee roster for space..." << "\n";
+            }
+            else {
+                cout << "Quitting...";
+                return 0;
+            }
+        }
+        cout << "Roster full. No more employee IDs can be added.";
+
+    }
+    if (menuChoice == 2)
+    {
+        // cout << "You selected menu choice 2." << "\n";    
+
+        cout << "\n" << "Enter the searchkey." << "\n";
+        cin >> searchkey;
+
+        bool result = search(employeeArray, rosterSize, searchkey);
+
+        if (result == true)
+        {
+            cout << "Employee ID " << searchkey << " has already completed the required training." << "\n";
+        }
+
+
+        else
+        {
+            cout << "Employee ID " << searchkey << " must complete the required training" << "\n";
+        }
+
+    }
+
+
+    else {
+
+        return 0;
+    }
+
 }
-
-/*
-Add function: (5 points)
-
-    add one employee id number to the array in the specified emp_index position
-    Required parameters: array, emp_index, employeeid
-
-Search function: (5 points)
-
-    sort the array, then search for a specific employee id.
-    Required parameters: array, size, searchkey
-    Required return value: true if the searchkey if found, or false if not found
-
-main function: (10 points)
-
-    Create an array and a variable to hold the size of the array.
-    Create a menu with 2 options: 
-    "1. Add employee to training roster" and  
-    "2. Search for an employee on the training roster" . 
-    Note, this menu does not need to be in a loop, but it would be a nice enhancement if you are up to the challenge.
-    Prompt the user to select a menu option.
-    If the user chooses to add an employee, 
-    you will need a variable called emp_index to track the number of employees that are added to the roster.  
-    Run a loop for input of employee ids in the main function. Use a while loop or a do while loop (do not use a for loop).
-        Prompt the user to input the employee id until the user inputs -1.
-        If the array is not full, call the add function in the body of the loop, pass the required arguments, then increment the emp_index variable.
-        If the array is full, display the message "Roster full" and break out of the loop.
-    If the user chooses to search for an employee, ask the user for the employee id.  
-    Call the search function and pass the required arguments.  Display a message depending on the return value of the search function.  For example, if the searchkey was 8345, the following message should display if the search function returns true: "Employee id 8345 has already completed the required training". Otherwise, display a different message.  For example, if the searchkey is 8345, the following message should display if the search function returns false: "Employee id 8345 must complete the required training".
-
-
-*/
